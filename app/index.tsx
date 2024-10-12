c5349f35a717cc2ae0195ad02328d8c3a3fbbc08
nhanbui1512 <buithiennhan0345@gmail.com>
2024-10-12 15:33:32 +0700

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,83 @@
+import { Link } from "expo-router";
+import { Button, Text, TextInput, TouchableOpacity, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={{ paddingTop: 50 }}>
+      <View
+        style={{
+          width: "100%",
+          paddingHorizontal: 20,
+          display: "flex",
+          justifyContent: "center",
+          marginTop: 200,
+        }}
+      >
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            paddingVertical: 20,
+          }}
+        >
+          <Text style={{ fontSize: 40, textAlign: "center", marginBottom: 40 }}>
+            SIGN IN
+          </Text>
+          <View style={{ paddingHorizontal: 20 }}>
+            <TextInput
+              placeholder="Email"
+              style={{
+                width: "100%",
+                fontSize: 20,
+                height: 50,
+                backgroundColor: "#fff",
+                borderRadius: 40,
+                paddingHorizontal: 20,
+                borderWidth: 1,
+                borderColor: "#ded2b8",
+                color: "rgb(58, 57, 103);",
+                marginBottom: 20,
+              }}
+            />
+
+            <TextInput
+              placeholder="Password"
+              style={{
+                width: "100%",
+                fontSize: 20,
+                height: 50,
+                backgroundColor: "#fff",
+                borderRadius: 40,
+                paddingHorizontal: 20,
+                borderWidth: 1,
+                borderColor: "#ded2b8",
+                color: "rgb(58, 57, 103);",
+                marginBottom: 20,
+              }}
+            />
+            <View style={{ display: "flex", flexDirection: "row", gap: 5 }}>
+              <Text>Don't have an account?</Text>
+              <Link href={"/register"} style={{ fontWeight: "600" }}>
+                Register Here
+              </Link>
+            </View>
+
+            <TouchableOpacity style={{ marginTop: 30 }}>
+              <View
+                style={{
+                  paddingVertical: 15,
+                  paddingHorizontal: 30,
+                  backgroundColor: "#000",
+                }}
+              >
+                <Text style={{ color: "#fff", textAlign: "center" }}>
+                  LOGIN
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
---
+++ app/register.tsx
@@ -0,0 +1,115 @@
+import { Link } from "expo-router";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+
+export default function Register() {
+  return (
+    <View style={{ paddingTop: 50 }}>
+      <View
+        style={{
+          width: "100%",
+          paddingHorizontal: 20,
+          display: "flex",
+          justifyContent: "center",
+          marginTop: 100,
+        }}
+      >
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            paddingVertical: 20,
+          }}
+        >
+          <Text style={{ fontSize: 40, textAlign: "center", marginBottom: 40 }}>
+            SIGN UP
+          </Text>
+          <View style={{ paddingHorizontal: 20 }}>
+            <TextInput
+              placeholder="Username"
+              style={{
+                width: "100%",
+                fontSize: 20,
+                height: 50,
+                backgroundColor: "#fff",
+                borderRadius: 40,
+                paddingHorizontal: 20,
+                borderWidth: 1,
+                borderColor: "#ded2b8",
+                color: "rgb(58, 57, 103);",
+                marginBottom: 20,
+              }}
+            />
+
+            <TextInput
+              placeholder="Email"
+              style={{
+                width: "100%",
+                fontSize: 20,
+                height: 50,
+                backgroundColor: "#fff",
+                borderRadius: 40,
+                paddingHorizontal: 20,
+                borderWidth: 1,
+                borderColor: "#ded2b8",
+                color: "rgb(58, 57, 103);",
+                marginBottom: 20,
+              }}
+            />
+
+            <TextInput
+              placeholder="Password"
+              style={{
+                width: "100%",
+                fontSize: 20,
+                height: 50,
+                backgroundColor: "#fff",
+                borderRadius: 40,
+                paddingHorizontal: 20,
+                borderWidth: 1,
+                borderColor: "#ded2b8",
+                color: "rgb(58, 57, 103);",
+                marginBottom: 20,
+              }}
+            />
+
+            <TextInput
+              placeholder="Confirm password"
+              style={{
+                width: "100%",
+                fontSize: 20,
+                height: 50,
+                backgroundColor: "#fff",
+                borderRadius: 40,
+                paddingHorizontal: 20,
+                borderWidth: 1,
+                borderColor: "#ded2b8",
+                color: "rgb(58, 57, 103);",
+                marginBottom: 20,
+              }}
+            />
+            <View style={{ display: "flex", flexDirection: "row", gap: 5 }}>
+              <Text>Already have an account?</Text>
+              <Link href={"/"} style={{ fontWeight: "600" }}>
+                Login
+              </Link>
+            </View>
+
+            <TouchableOpacity style={{ marginTop: 30 }}>
+              <View
+                style={{
+                  paddingVertical: 15,
+                  paddingHorizontal: 30,
+                  backgroundColor: "#000",
+                }}
+              >
+                <Text style={{ color: "#fff", textAlign: "center" }}>
+                  REGISTER
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
